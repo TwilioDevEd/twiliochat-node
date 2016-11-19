c8c7124fe803e51a2fdb34f84c227942c0813d87
juancarlospaco <juancarlospaco@gmail.com>
2016-11-18 23:02:53 -0300

    Fix code to work again with next gen libs 3.x.

--- app.js
+++ app.js
@@ -14,8 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -41,7 +40,7 @@ if (app.get('env') === 'development') {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: err
+      error: err,
     });
   });
 }
@@ -52,7 +51,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
-    error: {}
+    error: {},
   });
 });
 
--- routes/token.js
+++ routes/token.js
@@ -3,16 +3,16 @@ var router = express.Router();
 var TokenService = require('../services/tokenService');
 
 // POST /token
-router.post('/', function (req, res) {
+router.post('/', function(req, res) {
   var deviceId = req.body.device;
   var identity = req.body.identity;
   var tokenService = new TokenService();
 
-  var token = tokenService.generate(identity, deviceId)
+  var token = tokenService.generate(identity, deviceId);
 
   res.send({
     identity: identity,
-    token: token.toJwt()
+    token: token.toJwt(),
   });
 });
 
