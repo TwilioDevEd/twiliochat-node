a65f1d92e0784a5fa0d3c5f903292c6217f86069
José Oliveros <jose.oliveros.1983@gmail.com>
2016-12-06 16:47:20 -0600

    Clean up promises in channel setup

--- public/js/twiliochat.js
+++ public/js/twiliochat.js
@@ -91,12 +91,18 @@ var twiliochat = function() {
   function connectMessagingClient(tokenResponse) {
     // Initialize the IP messaging client
     tc.accessManager = new Twilio.AccessManager(tokenResponse.token);
-    tc.messagingClient = new Twilio.Chat.Client(tokenResponse.token);
-    updateConnectedUI();
-    tc.loadChannelList(tc.joinGeneralChannel);
-    tc.messagingClient.on('channelAdded', $.throttle(tc.loadChannelList));
-    tc.messagingClient.on('channelRemoved', $.throttle(tc.loadChannelList));
-    tc.messagingClient.on('tokenExpired', refreshToken);
+    tc.messagingClient = new Twilio.Chat.Client(tokenResponse.token, {logLevel: 'debug'});
+
+    tc.messagingClient.initialize()
+      .then(function() {
+        return tc.loadChannelList(tc.joinGeneralChannel);
+      })
+      .then(function() {
+        updateConnectedUI();
+        tc.messagingClient.on('channelAdded', $.throttle(tc.loadChannelList));
+        tc.messagingClient.on('channelRemoved', $.throttle(tc.loadChannelList));
+        tc.messagingClient.on('tokenExpired', refreshToken);
+      });
   }
 
   function refreshToken() {
@@ -122,7 +128,7 @@ var twiliochat = function() {
       return;
     }
 
-    tc.messagingClient.getPublicChannels().then(function(channels) {
+    return tc.messagingClient.getPublicChannels().then(function(channels) {
       tc.channelArray = tc.sortChannelsByName(channels);
       $channelList.text('');
       tc.channelArray.forEach(addChannel);
@@ -153,32 +159,40 @@ var twiliochat = function() {
   };
 
   function setupChannel(channel) {
-    // Join the channel
-    channel.join().then(function(joinedChannel) {
-      console.log('Joined channel ' + joinedChannel.friendlyName);
-      leaveCurrentChannel();
-      updateChannelUI(channel);
-      tc.currentChannel = channel;
-      tc.loadMessages();
-      channel.on('messageAdded', tc.addMessageToList);
-      channel.on('typingStarted', showTypingStarted);
-      channel.on('typingEnded', hideTypingStarted);
-      channel.on('memberJoined', notifyMemberJoined);
-      channel.on('memberLeft', notifyMemberLeft);
-      $inputText.prop('disabled', false).focus();
-      tc.$messageList.text('');
-    });
+    return leaveCurrentChannel()
+      .then(function() {
+        return tc.messagingClient.getChannelBySid(channel.sid)
+        .then(function(_channel) {
+          // Join the channel
+          return _channel.join()
+            .then(function(joinedChannel) {
+              console.log('Joined channel ' + joinedChannel.friendlyName);
+              updateChannelUI(_channel);
+              tc.currentChannel = _channel;
+              return tc.loadMessages();
+            })
+            .then(function() {
+              _channel.on('messageAdded', tc.addMessageToList);
+              _channel.on('typingStarted', showTypingStarted);
+              _channel.on('typingEnded', hideTypingStarted);
+              _channel.on('memberJoined', notifyMemberJoined);
+              _channel.on('memberLeft', notifyMemberLeft);
+              $inputText.prop('disabled', false).focus();
+            });
+        });
+      });
   }
 
   tc.loadMessages = function() {
-    tc.currentChannel.getMessages(MESSAGES_HISTORY_LIMIT).then(function(messages) {
+    return tc.currentChannel.getMessages(MESSAGES_HISTORY_LIMIT).then(function(messages) {
+      tc.$messageList.text('');
       messages.items.forEach(tc.addMessageToList);
     });
   };
 
   function leaveCurrentChannel() {
     if (tc.currentChannel) {
-      tc.currentChannel.leave().then(function(leftChannel) {
+      return tc.currentChannel.leave().then(function(leftChannel) {
         console.log('left ' + leftChannel.friendlyName);
         leftChannel.removeListener('messageAdded', tc.addMessageToList);
         leftChannel.removeListener('typingStarted', showTypingStarted);
@@ -186,6 +200,8 @@ var twiliochat = function() {
         leftChannel.removeListener('memberJoined', notifyMemberJoined);
         leftChannel.removeListener('memberLeft', notifyMemberLeft);
       });
+    } else {
+      return Promise.resolve();
     }
   }
 
