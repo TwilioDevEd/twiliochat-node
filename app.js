e4b96f16ebbb37cf323b7735153edb4491f3ddb8
José Oliveros <jose.oliveros.1983@gmail.com>
2016-12-06 09:13:39 -0600

    Disable dotenv when NODE_ENV=test

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'test') {
+  require('dotenv').config();
+}
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
