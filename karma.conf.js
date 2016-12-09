2fb1ffd9a6fc7a33ca41732667b43c2ff38fc9bf
juancarlospaco <juancarlospaco@gmail.com>
2016-12-09 15:08:59 -0300

    Fix for consistency we use similar Reporter for mocha and karma

--- karma.conf.js
+++ karma.conf.js
@@ -40,7 +40,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['mocha'],
 
 
     // web server port
@@ -72,5 +72,5 @@ module.exports = function(config) {
     // Concurrency level
     // how many browser should be started simultaneous
     concurrency: Infinity,
-  })
-}
+  });
+};
